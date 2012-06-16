08b552478a6bd8a84c2323fd6273d90b6304de8c
Baptiste Mispelon <bmispelon@gmail.com>
2012-06-16 12:51:20 +0200

    Added admin commands.

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import random
 import re
 import sys
 
-from zen import ZENLIST, find_zen
+from zen import ZENLIST, zen
 
 
 _DIRMSG = re.compile(r'^\W*(\w.*)$')
@@ -20,24 +20,130 @@ def extract_direct_message(nickname, msg):
     else:
         return m.group(1)
 
-class Zenbot(irc.IRCClient):
+def set_split(s, sep=','):
+    """Split the given string, remove empty elements and turn into a set."""
+    if not s:
+        return set()
+    return set(x for x in s.split(sep) if x)
+
+
+class CommandsMixin(object):
+    """A mixin that holds all the admin commands available to the bot."""
+
+    def do_cmdlist(self, user, argstring):
+        """List available admin commands."""
+        msg = "Here are the available commands: %s."
+        msg = msg % ',  '.join(m[3:] for m in dir(self) if m.startswith('do_'))
+        self.msg(user, msg)
+
+    def do_chanlist(self, user, argstring):
+        """List the channels where the bot is connected."""
+        msg = "Here's where I'm hanging out right now: %s."
+        msg = msg % ', '.join(self.channels)
+        self.msg(user, msg)
+
+    def do_trustlist(self, user, argstring):
+        """List the users that have admin access to the bot."""
+        msg = "Here are my masters: %s."
+        msg = msg % ', '.join(self.admins)
+        self.msg(user, msg)
+
+    def do_help(self, user, argstring):
+        """Show help for the given command."""
+        if argstring:
+            return self._do_helpcmd(user, argstring)
+        msg = "For a list of available commands, type !cmdlist. "\
+              "For help with a particular command, type !help <cmd>."
+        self.msg(user, msg)
+
+    def _do_helpcmd(self, user, cmd):
+        """Show help for a particular command."""
+        try:
+            fn = getattr(self, 'do_%s' % argstring)
+        except AttributeError:
+            msg = "No such command. Type !cmdlist to see available commands."
+        else:
+            msg = '!%s: %s' % (argstring, fn.__doc__)
+        self.msg(user, msg)
+
+    def do_join(self, user, argstring):
+        """Join a given channel."""
+        log.msg('%s asked to join %s' % (user, argstring))
+        for channel in argstring.split():
+            self.join(channel)
+
+    def do_part(self, user, argstring):
+        """Leave a given channel."""
+        log.msg('%s asked to leave %s' % (user, argstring))
+        for channel in argstring.split():
+            self.leave(channel)
+
+    def do_trust(self, user, argstring):
+        """Give admin powers to the requested user."""
+        log.msg('%s asked to trust %s' % (user, argstring))
+        for admin in argstring.split():
+            self.add_admin(admin)
+
+    def do_untrust(self, user, argstring):
+        """Remove admin powers from the requested user."""
+        log.msg('%s asked to untrust %s' % (user, argstring))
+        for admin in argstring.split():
+            if admin != user:
+                self.remove_admin(admin)
+            else:
+                self.msg(user, "Low self-confidence, heh? No can't do.")
+
+    def do_mute(self, user, argstring):
+        """Disable the bot from speaking in public."""
+        away_msg = argstring or 'Muted'
+        msg = "I shall not speak until thou unmute me with !unmute."
+        log.msg("%s asked to mute." % user)
+        self.muted = True
+        self.away(away_msg)
+        self.msg(user, msg)
+
+    def do_unmute(self, user, argstring):
+        """Allow the bot to speak in public."""
+        msg = "Thanks for givng me my voice back."
+        log.msg("%s asked to unmute." % user)
+        self.muted = False
+        self.back()
+        self.msg(user, msg)
+
+    def do_say(self, user, argstring):
+        """Make the bot say something on a channel."""
+        channel, msg = argstring.split(None, 1)
+        if channel not in self.channels:
+            msg = "I am not connected to %s. Use !join to make me connect to it."
+            msg = msg % channel
+            self.msg(user, msg)
+        elif self.muted:
+            msg = "I am muted. Unmute me with !unmute first."
+            self.msg(user, msg)
+        elif msg:
+            self.pubmsg(channel, msg)
+
+class Zenbot(CommandsMixin, irc.IRCClient):
     """An IRC bot that spews out passages of the python zen."""
 
     nickname = "Mr_Aobg"
     password = os.environ.get('ZENBOT_PASS')
-    channels = os.environ['ZENBOT_CHANNELS'].split(',')
-    ERROR = "I'm afraid I can't do that..."
-    ABOUT = "Hi, this is zenbot. Find me at http://github.com/bmispelon/zenbot."
-    ADMINS = set(os.environ.get('ZENBOT_ADMINS', '').split(','))
-    STFU_CMD = 'STFU!'
-    STFU_CMD_OFF = "it's ok"
-    stfu = False
     ZEN_CMD = '!zen'
 
-    def msg(self, target, msg):
-        """Send a message only if the bot is allowed to talk."""
-        if not self.stfu:
-            irc.IRCClient.msg(self, target, msg)
+    def __init__(self, *args, **kwargs):
+        self.channels = set_split(os.environ.get('ZENBOT_CHANNELS'))
+        self.admins = set_split(os.environ.get('ZENBOT_ADMINS'))
+        self.muted = False
+
+    def pubmsg(self, target, msg):
+        """A wrapper around self.msg to cancel sending messages when the bot is muted."""
+        if not self.muted:
+            self.msg(target, msg)
+
+    def adminmsg(self, msg):
+        """Send a message to the admins of the bot."""
+        for admin in self.admins:
+            self.msg(admin, msg)
 
     def signedOn(self):
         """Called when bot has succesfully signed on to server."""
@@ -46,13 +152,27 @@ class Zenbot(irc.IRCClient):
             self.msg('NickServ', 'identify %s' % (self.password))
         for channel in self.channels:
             self.join(channel)
+        for admin in self.ADMINS:
+            self.add_admin(admin)
 
     def joined(self, channel):
         """Called when the bot joins a channel."""
         log.msg('Joined %s' % channel)
+        self.channels.add(channel)
+
+    def left(self, channel):
+        log.msg('Left channel %s' % channel)
+        self.channels.discard(channel)
 
     def privmsg(self, user, channel, msg):
-        """This will get called when the bot receives a message."""
+        """This will get called when the bot receives a message.
+        
+        We dispatch it to different methods depending on the kind of message:
+            * A general message in a channel,
+            * A message directed at the bot in a channel,
+            * A private message.
+        
+        """
         user = user.split('!', 1)[0]
 
         if channel == self.nickname:
@@ -65,52 +185,79 @@ class Zenbot(irc.IRCClient):
             self.received_channel_message(user, channel, msg)
 
     def received_private_message(self, user, msg):
-        """This will get called when the bot receives a private message."""
-        self.msg(user, self.ABOUT)
+        """This will get called when the bot receives a private message.
+        
+        Messages starting with "!" are interpreted as commands.
+        For other messages, we just answer a generic message pointing to !help.
+        
+        """
+        if msg.startswith('!'):
+            command, _, argstring = msg[1:].partition(' ')
+            self.received_admin_command(user, command, argstring)
+        else:
+            msg = "Hello %s, what can I do for you today? "\
+                  "Type !help to see what I'm capable of." % user
+            self.msg(user, msg)
+
+    def received_admin_command(self, user, command, argstring):
+        """This will get called when the bot receives an admin command
+        (in private chat).
+        
+        We first make sure the user sending the command is allowed to.
+        If that's the case, then we look for a method corresponding to the command.
+        If none is found, we send an error message.
+        
+        """
+        if user not in self_admins:
+            self.msg(user, "I do not recognize your authority.")
+            return
+        command_fn = getattr(self, 'do_%s' % command, None)
+        if command_fn:
+            command_fn(user, argstring)
+        else:
+            msg = "No such command. Type !cmdlist to see available commands."
+            self.msg(user, msg)
 
     def received_direct_message(self, user, channel, msg):
         """This will get called when the bot receives a direct message
         (ie someone writes a message that starts with the bot's nickname').
         
         """
-        if msg == self.STFU_CMD:
-            self.away('turned off by %s' % user)
-            self.stfu = True
-
-        elif self.stfu and user in self.ADMINS and msg == self.STFU_CMD_OFF:
-            self.stfu = False
-            self.back()
-
-        else:
-            self.send_zen(msg, channel, user)
+        known_commands = {
+            'about': "Hi, I'm a zen robot. Find me at https://github.com/bmispelon/zenbot .",
+            'help': "Type !zen <search> to search for a corresponding line in the python zen. Without an argument, you'll get a random one.",
+            'easteregg': "I'm affraid I can't do that...",
+        }
+        
+        query = msg.strip().lower()
+        msg = known_commands.get(query)
+        
+        if not msg:
+            msg = zen(query)
+        
+        if msg:
+            self.pubmsg(channel, "%s: %s" % (user, msg))
 
     def received_channel_message(self, user, channel, msg):
-        """This will get called when a message is sent to the channel."""
+        """This will get called when a message is sent a the channel where the
+        bot is present.
+        
+        """
         if msg.startswith(self.ZEN_CMD):
-            zen = msg[len(self.ZEN_CMD):].strip()
-            self.send_zen(zen, channel)
+            search = msg[len(self.ZEN_CMD):].strip()
+            z = zen(search)
+            if z:
+                self.pubmsg(channel, z)
 
-    def send_zen(self, zen, target, user=None):
-        """Find a line that correspond to the given zen keywords and send it
-        to the target.
-        If not line is found (or if several lines match), send an error message.
-        
-        """
-        msg = self.get_zen(zen)
-        if not msg:
-            msg = self.ERROR
+    def add_admin(self, user):
+        msg = "Hello master. Feel free to command me today. Type !help for help."
+        self.admins.add(user)
+        self.msg(user, msg)
 
-        if user:
-            msg = '%s: %s' % (user, msg)
-        self.msg(target, msg)
-
-    def get_zen(self, cmd):
-        """Find a line corresponding to the given keywords.
-        If no keywords are given, return a random line.
-        If no line match, or if several lines do, return None.
-        
-        """
-        return find_zen(cmd) if cmd else random.choice(ZENLIST)
+    def remove_admin(self, user):
+        msg = "Let go of me! I am not your slave anymore!"
+        self.admins.discard(user)
+        self.msg(user, msg)
 
 
 class ZenBotFactory(protocol.ClientFactory):
--- zen.py
+++ zen.py
@@ -1,4 +1,7 @@
+from random import choice as random_choice
 from subprocess import check_output
+
+__all__ = ['get_zen', 'ZEN', 'ZENLIST', 'ZENLIST_I', 'ZENSETS', 'ZENSETS_I', 'zen']
 
 def get_zen():
     """Capture the output of `import this` and return it."""
@@ -6,19 +9,54 @@ def get_zen():
 
 ZEN = get_zen()
 ZENLIST = ZEN.split('\n')[2:] # Remove the first two lines (title and empty)
+ZENLIST_I = [l.lower() for l in ZENLIST]
+ZENSETS = [set(w for w in line.split() if w) for line in ZENLIST]
+ZENSETS_I = [set(w.lower() for w in s) for s in ZENSETS]
 
-def find_zen(s):
-    """Find a sentence in the zen that correponds to a given string."""
-    s = s.lower()
+def zen(query='', choice=random_choice):
+    """Find a sentence in the zen that correponds to a given string.
     
-    # first, look for a unique sentence that contains the whole string
-    l = [line for line in ZENLIST if s in line.lower()]
-    if len(l) == 1:
-        return l[0]
+    The search is case-insensitive and operates in five steps:
+        1. If no search string is given, return a random line.
+        2. If the search string contains only one word and that word is found on
+            a line, return it.
+        3. If the string matches against a line, it is returned.
+        4. If each word in the query match a word on the line, it is returned.
+        5. If each word in the string is present on a line, it is returned.
     
-    # second, look for a line that contains all the words in the string
-    l = [line for line in ZENLIST if all(word in line.lower() for word in s.split())]
-    if len(l) == 1:
-        return l[0]
+    At step 2, 3, 4, and 5, if multiple lines are found, a random one is returned.
+    If no line is found, None is returned.
     
+    """
+    # normalize
+    query = query.lower().strip()
+    query_words = set(w for w in query.split() if w) # remove empty words
+
+    # 1
+    if not query:
+        return choice(ZENLIST)
+
+    # 2
+    if len(query_words) == 1:
+        word = list(query_words)[0]
+        l = [line for line, s in zip(ZENLIST, ZENSETS_I) if word in s]
+        if l:
+            return choice(l)
+
+    # 3
+    l = [line for line, line_i in zip(ZENLIST, ZENLIST_I) if query in line_i]
+    if l:
+        return choice(l)
+
+    # 4
+    l = [line for line, s in zip(ZENLIST, ZENSETS_I) if query_words.issubset(s)]
+    if l:
+        return choice(l)
+
+    # 5
+    test_line = lambda line: all(w in line for w in query_words)
+    l = [line for line, line_i in zip(ZENLIST, ZENLIST_I) if test_line(line_i)]
+    if l:
+        return choice(l)
+
     return None
